fe0eed2a79a5577c5696a0cfedcf96194071e252
leo60228 <iakornfeld@gmail.com>
2020-11-16 17:55:43 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NetcoredbgSegfault
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Task.Run(async () => await Method(1));
+            await Task.Run(async () => await Method(2));
+        }
+
+        static async Task Method(int num) {
+            Console.WriteLine("Sleeping...");
+            await Task.Delay(500);
+            Console.WriteLine(num);
+        }
+    }
+}
